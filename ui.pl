d9d08128c81ff699467cb696a44091e06e3f973a
Xue Qi Xu <xueqixu@Xues-MacBook-Pro.local>
2013-11-27 14:33:30 -0800

    UI improved.

--- ui.pl
+++ ui.pl
@@ -43,14 +43,85 @@ gameOption :-
 	writeln('1. Set Current Room'),
 	writeln('2. Make Suggestion'),
 	writeln('3. Other player Suggestion'),
-	writeln('4. View Known Cards'),
-	writeln('5. Make Accusation'),
+	writeln('4. View Record'),
+	writeln('5. Set Values (Admin)'),
+	writeln('6. Delete Values (Admin)'),
 	read(X),
  	(X = 1 -> setCurrentLocation;
  		X = 2 -> makeSuggestion;
 		X = 3 -> setSuggestion;
-		X = 4 -> listing(knowncard), nl, gameOption;
-		X = 5 -> makeAccusation).
+		X = 4 -> subOption4;
+		X = 5 -> subOption5;
+		X = 6 -> subOption6).
+
+subOption4 :-
+	writeln('a. view knowncard'),
+	writeln('b. view doesNotHave'),
+	writeln('c. view couldHave'),
+	writeln('d. return to main menu'),
+	read(X),
+ 	(X = a -> subOption4a, nl, subOption4;
+ 		X = b -> subOption4b, nl, subOption4;
+		X = c -> subOption4c, nl, subOption4;
+ 		X = d -> gameOption).
+
+subOption4a :-
+	writeln('a. view knowncard by player'),
+	writeln('b. view knowncard by cardtype'),
+	writeln('c. return to Option 4'),
+	read(X),
+ 	(X = a -> listing(knowncard), nl, subOption4a;
+ 		X = b -> listing(knowncard), nl, subOption4a;
+ 		X = c -> subOption4).
+
+subOption4b :- 
+	writeln('a. display all doesNotHave'),
+	writeln('b. display cards player X does not have'),
+	writeln('c. display players who do not have Card'),
+	writeln('d. return to Option 4'),
+	read(X),
+ 	(X = a -> listing(doesNotHave), nl, subOption4b;
+ 		X = b -> writeln('b. display cards player X does not have'), nl, subOption4b;
+ 		X = c -> writeln('c. display players who do not have Card'), nl, subOption4b;
+ 		X = d -> subOption4).
+
+subOption4c :-
+	writeln('a. display all couldHave'),
+	writeln('b. display all cards player X might have'),
+	writeln('c. display all players who might have Card'),
+	writeln('d. return to Option 4'),
+	read(X),
+ 	(X = a -> listing(couldHave), nl, subOption4c;
+ 		X = b -> writeln('b. display all cards player X might have'), nl, subOption4c;
+ 		X = c -> writeln('c. display all players who might have Card'), nl, subOption4c;
+ 		X = d -> subOption4).
+
+subOption5 :-
+	writeln('a. assert knowncard'),
+	writeln('b. assert doesNotHave'),
+	writeln('c. assert couldHave'),
+	writeln('d. assert goal card'),
+	writeln('e. return to main menu'),
+	read(X),
+ 	(X = a -> setKnownCards, nl, subOption5;
+ 		X = b -> set_does_not_have, nl, subOption5;
+		X = c -> writeln('c. assert couldHave'), nl, subOption5;
+		X = d -> writeln('d. assert goal card'), nl, subOption5;
+		X = e -> gameOption).
+
+subOption6 :-
+	writeln('a. retract knowncard'),
+	writeln('b. retract doesNotHave'),
+	writeln('c. retract couldHave'),
+	writeln('d. retract goal card'),
+	writeln('e. return to main menu'),
+	read(X),
+ 	(X = a -> retractall(knowncard(_,_)), nl, subOption5;
+ 		X = b -> retractall(doesNotHave(_,_)), nl, subOption5;
+		X = c -> retractall(couldHave(_,_,_,_)), nl, subOption5;
+		X = d -> retractall(goalCard(_)), nl, subOption5;
+		X = e -> gameOption).
+	
 
 makeSuggestion :-
 	findall(X, person(X),Lperson), % all valid persons
